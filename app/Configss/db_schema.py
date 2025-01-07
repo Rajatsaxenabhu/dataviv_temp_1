6de86020578b75d74c3859cc178fd08406cc5eaa
Rajat saxena <rajat@gmail.com>
2025-01-07 12:22:03 +0530

    temp

--- app/Configss/db_schema.py
+++ app/Configss/db_schema.py
@@ -1,77 +1,116 @@
-from sqlalchemy import Integer, String, ForeignKey, DateTime, Enum
+from sqlalchemy import Integer, String, ForeignKey, DateTime, Enum, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from datetime import datetime, timezone
-from enum import Enum
+from typing import List
 
 class Base(DeclarativeBase):
     pass
 
-
 class Task(Base):
-    __tablename__ = 'tasks'
+    __tablename__ = "tasks"
     
     task_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    file_name: Mapped[str] = mapped_column(String(40), nullable=False)
+    file_name: Mapped[str] = mapped_column(String(40), nullable=False, unique=True)
+    task_internal_id: Mapped[str | None] = mapped_column(String(40))
+    
     created_at: Mapped[datetime] = mapped_column(
-        DateTime,
+        DateTime(timezone=True),
         nullable=False,
-        default=datetime.now(timezone.utc)
+        server_default=func.now()
     )
+    
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+    
+    progress: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=0
+    )
+
+    total_sub_tasks: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=0
+    )
+    
+    remaining_sub_tasks: Mapped[int] = mapped_column(
+        Integer,
+        nullable=False,
+        default=0
+    )
+    
     status: Mapped[str] = mapped_column(
         String(20),
         nullable=False,
-        default="other"
+        default="READY"
     )
     
-    # sub_tasks = relationship("Sub_task", back_populates="task")
+    sub_tasks: Mapped[List["Sub_task"]] = relationship(
+        "Sub_task",
+        back_populates="task",
+        cascade="all, delete-orphan"
+    )
     
-    def __init__(self, file_name: str, status: str = "other"):
+    def __init__(self, file_name: str, status: str,task_internal_id: str) -> None:
         self.file_name = file_name
         self.status = status
+        self.task_internal_id=task_internal_id
     
+    def set_status(self, new_status: str) -> None:
+        self.status = new_status
     
-    def set_status(self, new_status: str) -> None: 
-        if isinstance(new_status, str): 
-            self.status = new_status
-    
-    def __repr__(self):
-        return f"Task(task_id={self.task_id}, name={self.file_name}, created_at={self.created_at}, status={self.status})"
+    def __repr__(self) -> str:
+        return (f"Task(task_id={self.task_id}, "
+                f"file_name={self.file_name}, "
+                f"status={self.status})")
 
 
 
-# class Sub_task(Base):
-#     __tablename__ = 'sub_tasks'
+class Sub_task(Base):
+    __tablename__ = "sub_tasks"
     
-#     sub_task_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-#     task_id: Mapped[int] = mapped_column(
-#         Integer,
-#         ForeignKey('tasks.task_id', ondelete='CASCADE'),
-#         nullable=False
-#     )
-#     created_at: Mapped[datetime] = mapped_column(
-#         DateTime,
-#         nullable=False,
-#         default=datetime.now(timezone.utc)
+    sub_task_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    main_task_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("tasks.task_id", ondelete="CASCADE"),
+        nullable=False
+    )
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now()
+    )
+    
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+    
+    curr_status: Mapped[str] = mapped_column(
+        String(20),
+        nullable=False,
+        default="PENDING" # pending, running, success, failure
+    )
+    
+    task: Mapped["Task"] = relationship("Task", back_populates="sub_tasks")
+    
+    def __init__(self,main_task_id: str,curr_status: str) -> None:
+        self.main_task_id = main_task_id
+        self.curr_status = curr_status
 
-#     )
-#     updated_at: Mapped[datetime] = mapped_column(
-#         DateTime,
-#         nullable=False,
-#         default=datetime.now(timezone.utc),
-#         onupdate=datetime.now(timezone.utc)
-#     )
-#     curr_status: Mapped[Sub_status] = mapped_column(  #
-#         Enum(Sub_status),
-#         nullable=False,
-#         default=Sub_status.created
-#     )
+    def set_status(self, new_status: str) -> None:
+        self.curr_status = new_status
     
-#     task = relationship("Task", back_populates="sub_tasks")
-    
-    
-#     def set_status(self, new_status: Sub_status) -> None:  
-#         if isinstance(new_status, Sub_status): 
-#             self.curr_status = new_status
-    
-#     def __repr__(self):
-#         return f"Sub_task(sub_task_id={self.sub_task_id}, task_id={self.task_id}, created_at={self.created_at}, curr_status={self.curr_status})"
+    def __repr__(self) -> str:
+        return (f"Sub_task(sub_task_id={self.sub_task_id}, "
+                f"task_id={self.task_id}, "
+                f"created_at={self.created_at}, "
+                f"status={self.curr_status})")
--- app/main.py
+++ app/main.py
@@ -6,13 +6,9 @@ from datetime import datetime
 from Configss.config_schema import get_db
 from Configss.db_schema import Task
 from sqlalchemy_utils import database_exists, create_database
-import random
-import string
 app = FastAPI()
 
 
-def generate_random_suffix(length=5):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -20,23 +16,24 @@ async def root():
 @app.post("/uploadfile/")
 async def upload_file(file: UploadFile = File(...),db:Session=Depends(get_db)):
     new_file_name = file.filename
-    existing_task = db.query(Task).filter(Task.file_name == new_file_name).first()
-    if existing_task:
-        random_suffix = generate_random_suffix()
-        base_name, ext = file.filename.rsplit('.', 1)
-        new_file_name = f"{base_name}_{random_suffix}.{ext}"
-    
     file_location = f"./work_dir/{new_file_name}"    
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-    new_task = Task(file_name=new_file_name,status="pending")
+    
+    schedule_time=1 # in minutes
+    gap_time=10 # in seconds
+    tasky=main_task.apply_async(args=[file_location,schedule_time,gap_time])
+    print("this is the ",tasky.task_id)
+    new_task = Task(file_name=new_file_name,status="READY",task_internal_id=tasky.task_id)
     db.add(new_task)
     db.commit()
     db.refresh(new_task)
-    schedule_time=1 # in minutes
-    gap_time=10 # in seconds
-    main_task.apply_async(args=[file_location,schedule_time,gap_time])
     print("file uploaded successfully")
     return {"filename": file.filename}
+
+@app.get("/get_tasks/")
+async def get_tasks(db:Session=Depends(get_db)):
+    tasks = db.query(Task).all()
+    return {"tasks": tasks}
 
     
